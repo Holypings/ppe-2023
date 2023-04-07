d0f8c5523f885d2422700376d27a4ba6920318ef
Domyth <118464446+Domyth@users.noreply.github.com>
2023-04-07 17:20:18 +0200

    Add files via upload

---
+++ ppe-antoine/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <?php
+require_once("header.php")
+?>
+</head>
+
+
+
+
+<body>
+
+    <section>
+        <div id="acc">
+         <img src="img/accueil.png" width="100%" />
+        </div>
+    </section>
+
+</body>
+
+<footer>
+<?php
+require_once("footer.php")
+?>
+</footer>
+
+</html>
