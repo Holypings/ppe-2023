4b131307d1b953e6d476ec3c710f18037fc7cd74
Olivia <olivia.icard03@gmail.com>
2023-06-08 22:36:34 +0200

    maj

---
+++ achat.php
@@ -0,0 +1,65 @@
+<?php
+require_once "db.php";
+require_once "header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/achat.css" />
+  <title>Achat</title>
+</head>
+<body>
+  <div class="container">
+    <div class="form-container">
+      <h2>Phase d'achat</h2>
+      
+      <?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $card_number = $_POST['card_number'];
+        $card_name = $_POST['card_name'];
+        $expiration_date = $_POST['expiration_date'];
+        $cvv = $_POST['cvv'];
+
+        if (empty($card_number) || empty($card_name) || empty($expiration_date) || empty($cvv)) {
+          echo "<p style='color: red;'>Veuillez remplir tous les champs.</p>";
+        } elseif (!preg_match('/^\d{16}$/', $card_number)) {
+          echo "<p style='color: red;'>Le numéro de carte doit contenir 16 chiffres.</p>";
+        } elseif (!preg_match('/^[a-zA-Z]+$/', $card_name)) {
+          echo "<p style='color: red;'>Le nom du titulaire de la carte ne doit contenir que des lettres.</p>";
+        } elseif (!preg_match('/^(0[1-9]|1[0-2])\/(2[3-6])$/', $expiration_date)) {
+          echo "<p style='color: red;'>Le format de la date d'expiration doit être MM/YY et être compris entre 01/23 et 12/26.</p>";
+        } elseif (!preg_match('/^\d{3}$/', $cvv)) {
+          echo "<p style='color: red;'>Le CVV doit contenir exactement 3 chiffres.</p>";
+        } else {
+          echo "<p style='color: green;'>Merci de votre achat !</p>";
+          mysqli_query($conn, "DELETE FROM panier WHERE ID_USER = $userId AND ID_KB = $productId");
+          header("Refresh: 2; URL=index.php");
+          die();
+        }
+      }
+      ?>
+      
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="card_number">Numéro de carte :</label>
+        <input type="text" id="card_number" name="card_number" pattern="\d{16}" title="Le numéro de carte doit contenir 16 chiffres" required>
+        
+        <label for="card_name">Nom du titulaire de la carte :</label>
+        <input type="text" id="card_name" name="card_name" pattern="^[^\d]+$" title="Le nom doit contenir uniquement des lettres" required>
+        
+        <label for="expiration_date">Date d'expiration (MM/YY) :</label>
+        <input type="text" id="expiration_date" name="expiration_date" pattern="^(0[1-9]|1[0-2])\/(2[3-6])$" title="Le format de la date d'expiration doit être MM/YY et être compris entre 01/23 et 12/26" required>
+        
+        <label for="cvv">CVV (3 chiffres) :</label>
+        <input type="text" id="cvv" name="cvv" pattern="\d{3}" title="Le CVV doit contenir exactement 3 chiffres" required>
+        
+        <input type="submit" value="Valider le paiement">
+      </form>
+    </div>
+  </div>
+
+</body>
+</html>
+<?php
+require_once "footer.php";
+?>
---
+++ panier.php
@@ -0,0 +1,95 @@
+<?php
+require_once "header connecté.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Panier</title>
+</head>
+<body>
+    <h1>Votre Panier</h1>
+    <div id="products">
+        <?php
+require_once 'db.php';
+
+// Vérifier si l'utilisateur est connecté
+session_start();
+if (isset($_SESSION['ID'])) {
+ // récupérer les produits du panier de l'utilisateur depuis la base de données
+ $userId = $_SESSION['ID'];
+ // récupère ID depuis le panier
+ $sql = "SELECT ID_KB, Quantite FROM panier WHERE ID_USER = $userId";
+
+ $result = mysqli_query($conn, $sql);
+
+ // utilise l'ID_KB récupéré et l'assimile à l'id correspondant dans la table produit
+ // afficher les produits du panier
+ if (mysqli_num_rows($result) > 0) {
+  $prixb = 0;
+  ?>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Nom de l'article</th>
+                            <th>Prix de l'article</th>
+                            <th>Quantité(s)</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+while ($row = mysqli_fetch_assoc($result)) {
+   $test = $row["ID_KB"];
+   $sql2 = "SELECT nom, prix FROM produit WHERE ID_KB = $test";
+   $result2 = mysqli_query($conn, $sql2);
+   $row2 = mysqli_fetch_array($result2);
+   $img = "img/" . $test . ".png";
+   $nom = $row2["nom"];
+   $prix = $row2["prix"];
+   $quantite = $row["Quantite"];
+   $prixa = $prix * $quantite;
+   $prixb += $prixa;
+   ?>
+                            <tr>
+                                <td><img src="<?php echo $img; ?>" class="imgpa"/></td>
+                                <td><?php echo $nom ?></td>
+                                <td><?php echo $prix . " €"; ?></td>
+                                <td><?php echo $quantite; ?></td>
+                                <td>
+                                    <form method="post">
+                                        <input type="hidden" name="product_id" value="<?php echo $test; ?>">
+                                        <button type="submit" name="delete_product">Supprimer</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            <?php
+}
+  ?>
+                        <tr>
+                            <td colspan="3" style="text-align: right;"><strong>TOTAL :</strong></td>
+                            <td> <h3><?php echo $prixb . " €"; ?></h3></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br>
+                <button onclick="location.href='achat.php'">Commander</button>
+                <?php
+  // Supprimer un produit spécifique du panier
+  if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete_product'])) {
+    $product_id = $_POST['product_id'];
+    mysqli_query($conn, "DELETE FROM panier WHERE ID_USER = $userId AND ID_KB = $product_id");
+    echo "<p style='color: green;'>Le produit a été supprimé du panier.</p>";
+  }
+} else {
+  echo 'Votre panier est vide.';
+ }
+}
+
+// ferme connexion à la base de données
+mysqli_close($conn);
+?>
+    </div>
+</body>
+</html>
