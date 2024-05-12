1497850fdb92a39d9b230a861071039a1413aa29
Olivia <110298176+Holypings@users.noreply.github.com>
2024-05-12 12:01:06 +0200

    Update header connecté.php

--- header connecté.php
+++ header connecté.php
@@ -27,11 +27,36 @@ require_once("db.php");
         <a href="index.php"><img src="img/logo contour blanc.png" width="130" /></a>
     </div>
 
-    <div id="panier">
-        <a href="panier.php"> <img src="img/panier.png" width="50" /></a>
+    <div id="recherche">
+        <a href="recherche.php"> <img src="img/search.png" width="50" /></a>
         
     </div>
 
+    <?php
+    if (isset($_SESSION['ID'])) {
+    ?>
+    <div id="panier">
+        <a href="panier.php"> <img src="img/panier.png" width="50" /></a> 
+    </div>
+    <?php
+    }else{?>
+        <div id="panier">
+   
+        <a><img src="img/panier.png" title="Vous devez être connecté" width="50"/></a>
+            
+        </div>
+<?php
+    }
+?>
+    <?php
+    if (isset($_SESSION['ID'])) {
+    ?>
+    <div id="exit">
+        <a href="deconnexion.php"> <img src="img/exit.png" width="70" /></a>
+    </div>
+<?php
+    }
+?>
     
 
 
